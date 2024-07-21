f2df11860eaf8850473a79f5d68cec82a1b52c1d
Ana <anafdezostio01@gmail.com>
2024-07-21 15:20:47 +0200

    add SVG icons to languages

--- src/components/LanguageSelector.tsx
+++ src/components/LanguageSelector.tsx
@@ -2,12 +2,25 @@ import { useTranslation } from 'react-i18next';
 import { MenuItem, Select, FormControl, InputLabel } from '@mui/material';
 import { SelectChangeEvent } from '@mui/material';
 import { LENGAUGES_LIST } from '../utils/constants';
+import SpainFlag from '../icons/flags/SpainFlag';
+import EnglishFlag from '../icons/flags/EnglishFlag';
 
 const LanguageSelector: React.FC = () => {
   const { t, i18n } = useTranslation();
   
   const handleChange = (event: SelectChangeEvent<string>) => {
     i18n.changeLanguage(event.target.value);
+  };
+
+  const renderIcon = (code: string) => {
+    switch (code) {
+      case 'es':
+        return <SpainFlag width="20px" height="20px" style={{ marginRight: '8px' }} />;
+      case 'en':
+        return <EnglishFlag width="20px" height="20px" style={{ marginRight: '8px' }} />;
+      default:
+        return null;
+    }
   };
 
   return (
@@ -22,6 +35,7 @@ const LanguageSelector: React.FC = () => {
       >
         {LENGAUGES_LIST.map((language) => (
           <MenuItem key={language.code} value={language.code}>
+            {renderIcon(language.code)}
             {t(language.label)}
           </MenuItem>
         ))}
