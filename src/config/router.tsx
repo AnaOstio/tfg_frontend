557d81b3062c2c7cb9edb8614b27959c7da85937
Ana <anafdezostio01@gmail.com>
2025-05-18 12:38:24 +0200

    configure testing

--- src/config/router.tsx
+++ src/config/router.tsx
@@ -15,7 +15,7 @@ const TitleMemoryDetails = React.lazy(() => import("../pages/titleMemory/TitleMe
 const SubjectDetails = React.lazy(() => import("../pages/subjects/SubjectDetails"));
 const UploadSubjectsScreen = React.lazy(() => import("../pages/subjects/upload/UploadSubjectsScreen"));
 
-export const router = createBrowserRouter([
+const router = createBrowserRouter([
     {
         path: "/",
         element: <RootLayout />,
@@ -101,3 +101,5 @@ export const router = createBrowserRouter([
         ],
     },
 ]);
+
+export default router;
